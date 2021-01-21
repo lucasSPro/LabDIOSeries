a0751efabcc83f274b2031e5f0c3167efa94db8b
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-01-20 22:40:29 -0300

    implementa a classe Serie

--- DIO.Series/Classes/Serie.cs
+++ DIO.Series/Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
@@ -8,5 +10,36 @@ namespace DIO.Series
         private string Titulo {get; set;}
         private string Descricao {get; set;}
         private int Ano {get; set;}
+
+        // metodos
+        public Serie (int id, Genero  genero, string titulo, string descricao, int ano)
+        {
+            this.Id =  id;
+            this.Genero = genero;
+            this.Titulo =  titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.Ano;
+            
+            return retorno;
+        }
+
+        public int retornaId()
+        {
+            return  this.Id;
+        }
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
     }
+
 }
